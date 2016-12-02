f5ef2ad644538681c48571886b356a56f9852eff
Harry Epstein <something@somethingelse.com>
2016-12-02 12:24:41 -0600

    did some stuff; undid some stuf; committed anyway

--- quiz.js
+++ quiz.js
@@ -1,42 +1,47 @@
 /*EXAMPLE OBJECT*/
 let treeObject = {
-height: 5,
+height: 20,
 character: "*",
 }
 
 
-// function buildObject(){
+// function buildObject() NEED AN OBJECT CONSTRUCTOR
 
 
 // When the button is clicked or the return key is pressed create the input object and call buildAtree function which prints tree design to console
 
 var button = document.getElementById('grow-button')
 
-button.addEventListener('click', function (mouseEvt){
-  console.log(mouseEvt)
-  console.log("you clicked grow")
+button.addEventListener('click', function (mouseEvt){//deine input by getting element
+  console.log(mouseEvt);
+  console.log("you clicked grow");//grow baby, grow!
 
+  buildAtree(treeObject);//create tree
 })
 
-let input = document.getElementById("tree-height");
+//when a return is pressed inside the input field, run the buildAtree function
+
+let input = document.getElementById("tree-height");//deine input by getting element
 
 input.addEventListener('keypress', function (keyboardEvt) {
   console.log(keyboardEvt)
   if(keyboardEvt.key ==='Enter')
-    console.log("you pressed enter")
+    console.log("you pressed enter")//grow baby, grow!
 
-    buildAtree(treeObject);
+    buildAtree(treeObject);//create tree
 })
 
 
 function buildAtree (object){
-  for (let i = ""; i <= object.height; i += object.character)//for the height of the object increment i each time by the character in the objec
+  let tree = "";
+  for (let i = ""; i.length <= object.height; i += object.character)//for the height of the object increment i each time by the character in the objec
   {
-    if (i.length % 3 ===0)
-      console.log("Hello I am in the for loop, help me!")
-      console.log(i)//if i is odd, output i which should be the same number of character of the line number, which should form the correct tree structure, 1, 3, 5, 7, 9, 11
+    if (i.length % 3 ===0){
+      tree += i;
+    }  //if i is odd, output i which should be the same number of character of the line number, which should form the correct tree structure, 1, 3, 5, 7, 9, 11
   }
-  console.log(" ***** \n ***** \n *****\n *****\n *****\n *****\n *****\n" )//every tree needs a trunk
+  i += "***** \n ***** \n *****\n *****\n *****\n *****\n *****\n";
+  console.log(tree) //every tree needs a trunk
 }
 
-buildAtree(treeObject);
+buildAtree(treeObject);//CALL THE FUNCTION
